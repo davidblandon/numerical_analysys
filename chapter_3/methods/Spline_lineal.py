ac94264be1238cd9d9be018ab01154c2935f15c2
KatherinAllin28 <68928423+KatherinAllin28@users.noreply.github.com>
2024-05-28 18:14:53 -0500

    Update Spline_lineal.py

--- chapter_3/methods/Spline_lineal.py
+++ chapter_3/methods/Spline_lineal.py
@@ -1,26 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.interpolate import CubicSpline
+from scipy.interpolate import InterpolatedUnivariateSpline
 
 def spline_lineal(x, y):
-    cs = CubicSpline(x, y, bc_type='natural')
-    return cs
+    spl = InterpolatedUnivariateSpline(x, y, k=1)
+    return spl
 
-def graficar(x, y, cs):
+def graficar(x, y, spl):
     x_new = np.linspace(min(x), max(x), 100)
-    y_new = cs(x_new)
+    y_new = spl(x_new)
     plt.plot(x, y, 'o', label='Datos')
     plt.plot(x_new, y_new, label='Spline lineal')
     plt.legend()
     plt.show()
 
-def guardar_archivo(x, y, cs):
-    with open("resultado.txt", "w") as f:
+def guardar_archivo(x, y, spl):
+    with open("resultado_lineal.txt", "w") as f:
         f.write("Datos:\n")
         for i in range(len(x)):
             f.write(f"({x[i]:.2f}, {y[i]:.2f})\n")
         f.write("\nPolinomio:\n")
-        f.write(str(cs) + "\n")
+        f.write(str(spl) + "\n")
 
 def main():
     x = input("Ingrese los valores de x separados por coma: ")
@@ -28,10 +28,10 @@ def main():
     x = np.array([float(i) for i in x.split(',')])
     y = np.array([float(i) for i in y.split(',')])
     
-    cs = spline_lineal(x, y)
-    print(cs)
-    graficar(x, y, cs)
-    guardar_archivo(x, y, cs)
+    spl = spline_lineal(x, y)
+    print(spl)
+    graficar(x, y, spl)
+    guardar_archivo(x, y, spl)
 
 if __name__ == "__main__":
     main()
